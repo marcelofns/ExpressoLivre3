0603ecb76e8a8f9a4e66669ab11fd1fb9cea993b
Marcio Albuquerque <marcio.lima.albuquerque@gmail.com>
2012-07-30 16:45:11 -0300

    IM File Transfer v0.1

--- tine20/Messenger/js/Layout.js
+++ tine20/Messenger/js/Layout.js
@@ -247,6 +247,172 @@ Tine.Messenger.SimpleDialog = function(_config){
 
 var configWindow = null;
     
+<<<<<<< HEAD
+=======
+Tine.Messenger.Config = {
+    
+    ClientLayout : {
+            id:'ClientDialog',
+            title: 'Expresso Messenger',
+            iconCls: 'messenger-icon-off',
+            connected: false,
+            status: '',
+            width: 220,
+            minWidth: 220,
+            height: 420,
+            minHeight: 270,
+            closeAction: 'hide', //'close' - destroy the component
+            collapsible: true,
+            plain: true,
+            autoScroll:   true,
+            border: false,
+            layout: 'border', // 'fit'
+            listeners: {
+                move: function(_box){
+                    Tine.Messenger.Window._onMoveWindowAction(_box);
+                }
+            },
+            tbar: {
+                    cls: 'messenger-client-tbar',
+                    items:[
+                        {
+                            //text: Tine.Tinebase.appMgr.get('Messenger').i18n.ngettext('Action', 'Actions', 1),
+                            id: 'messenger-menu-actions',
+                            text: 'Actions',
+                            menu: {
+                                    id: "BuddysMenu",
+                                    items:[{
+                                            id: 'messenger-contact-add',
+                                            icon: '/images/messenger/user_add.png',
+                                            //text: Tine.Tinebase.appMgr.get('Messenger').i18n.ngettext('Add Contact', 'Add Contact', 1),
+                                            text: 'Add Contact',
+                                            disabled: true,
+                                            handler: function(){
+                                                Tine.Messenger.Window.AddBuddyWindow();
+                                            }
+                                    },
+                                    {
+                                            id: 'messenger-group-mngt-add',
+                                            //text: Tine.Tinebase.appMgr.get('Messenger').i18n.ngettext('Add Group', 'Add Group', 1),
+                                            text: 'Add Group',
+                                            icon: '/images/messenger/group_add.png',
+                                            disabled: true,
+                                            handler: function() {
+                                                new Tine.Messenger.WindowConfig(Tine.Messenger.Window.Groups).show();
+                                            }
+                                     },
+                                     {
+                                         id: 'messenger-logout',
+                                         //text: Tine.Tinebase.appMgr.get('Messenger').i18n.ngettext('Logout', 'Logout', 1),
+                                         text: 'Logout',
+                                         disabled: true,
+                                         handler: function() {
+                                             Tine.Messenger.ChatHandler.disconnect();
+                                         }
+                                     }
+                                    ]
+                                }
+                        },
+                        {
+                            //text: Tine.Tinebase.appMgr.get('Messenger').i18n.ngettext('Preference', 'Preferences', 1),
+                            id: 'messenger-menu-prefs',
+                            text: 'Preferences',
+                            handler: function () {
+                                if (configWindow == null)
+                                    configWindow = new Tine.Messenger.ConfigWindow();
+                                
+                                configWindow.show();
+                            }
+                        }]
+            },
+            items:[{
+			region:'center',
+			border:false,
+                        bodyStyle:'padding:6px 3px 0 3px;',
+			layout:'fit',
+                        items: Tine.Messenger._Roster
+                    } 
+                    ,{
+                        id: 'messenger-connect-display',
+                        html: '<img src="/images/messenger/loading_animation_liferay.gif" style="display:none" />',
+                        cls: 'messenger-connect-display',
+                        region:'center',
+                        border: false,
+                        buttons: [
+                                {
+                                    id: 'messenger-connect-cmd',
+                                    text: 'Connect',
+                                    region:'center',
+                                    cls: 'messenger-connect-cmd',
+                                    handler: function() {
+                                        Tine.Messenger.ChatHandler.connect();
+                                    }
+                                }
+                            ]
+                    }
+		]
+            ,bbar:{
+                id: 'status-container',
+                height: 27,
+                cls: 'messenger-client-tbar',
+                items: [{   
+                        id: 'messenger-change-status-button',
+                        icon: '/images/messenger/user_offline.png'
+                    },
+                    {xtype: 'tbspacer', width: 5},
+                    {
+                        xtype:'textfield',
+//                        store: Tine.Messenger.factory.statusStore,
+                        displayField:'text',
+                        width: 175,
+                        valueField:'value',
+                        typeAhead: true,
+                        name:'message',
+                        mode: 'local',
+                        triggerAction: 'all',
+                        id:'messenger-status-box',
+                        emptyText:'your Status... (press ENTER after)',
+                        selectOnFocus:true
+                    }
+                ]
+            },
+            html: '<iframe id="iframe-upload" src="/upload.html" style="display: none;"></iframe>' +
+                  '<iframe id="iframe-download" src="" style="display: none;"></iframe>'
+    }
+    
+    , ChatWindowLayout : {
+        iconCls:     'messenger-icon',
+        cls:         'messenger-chat-window',
+        width:       460,
+        minWidth:    400,
+        height:      360,
+        minHeight:   280,
+        closeAction: 'hide', //'close' - destroy the component
+        collapsible: true,
+        plain:       true,
+        layout:      'border',
+        listeners: {
+            beforerender: function(_box){
+                Tine.Messenger.AddItems(_box);
+            },
+            resize: function(_box, _width, _height){
+                Tine.Messenger.ChatHandler.adjustChatAreaHeight(_box.id, _width, _height);
+            },
+            show: function () {
+                this.setTextfieldFocus();
+            },
+            activate: function () {
+                this.setTextfieldFocus();
+            },
+            expand: function () {
+                this.setTextfieldFocus();
+            }
+        }
+  }
+    
+}
+
+>>>>>>> IM File Transfer v0.1
 Tine.Messenger.AddItems = function(_box) {
     if(!_box.items){
         var items = Array();
